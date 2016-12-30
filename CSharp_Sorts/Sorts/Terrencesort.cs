7276e0ab5046c1e865f0a6113618e27cf8a523e2
Garrett Wong <garrett.wong@wdc.com>
2016-12-30 10:32:39 -0800

    add .net regular sort

---
+++ CSharp_Sorts/Sorts/Terrencesort.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Sorts.Sorts
+{
+    public class DotNetArraySort : ISort
+    {
+        public void Sort(int[] arr)
+        {
+            Array.Sort(arr);
+        }
+    }
+}
